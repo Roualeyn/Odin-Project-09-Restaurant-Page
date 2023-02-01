385a866018c518fabf568e6e5c5dc1fa5176119e
Roualeyn <roualeyn@gmail.com>
2023-02-01 21:16:57 +0200

    Add Contact Page

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,8 @@
 export default function contact(container) {
-    alert("Not Yet Implemented");
+    let aboutHeader = document.createElement("h2");
+    aboutHeader.textContent = "Contact Us";
+    let aboutText = document.createElement("p");
+    aboutText.innerHTML = `Please contact us using our office number at <a href="tel:01234567891">12345678910.</a> Our opening hours are 9 to 5, Mondays through Friday.`;
+    container.appendChild(aboutHeader);
+    container.appendChild(aboutText);
 }
